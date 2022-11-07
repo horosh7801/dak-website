43151a89c9131b83cd372d4aa85107beb403caa3
Oleg Khoroshilov <horosh7801@gmail.com>
2022-11-08 02:05:35 +0500

    not compiling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import { css } from '@emotion/css'
+import { Roboto_Flex } from '@next/font/google'
+
+
+const roboto = Roboto_Flex({display: 'swap'})
+
+const header = css`
+  height: 50px;
+`
+const stickyHeader = css`
+  height: 30px;
+  background-color: #1a88ad;
+`
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={roboto.className}>
+      <div className={header}>
+
+      </div>
+      <Component {...pageProps} />
+    </div>  
+  )
 }
 
 export default MyApp
